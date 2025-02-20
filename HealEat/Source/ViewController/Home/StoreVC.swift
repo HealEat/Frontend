e1307d8641c3db8b8c3f4f3402e3144e33d2d744
mireat <162596683+mireat@users.noreply.github.com>
2025-02-20 19:47:42 +0900

    매장 리뷰 더미데이터 구현

--- HealEat/Source/ViewController/Home/StoreVC.swift
+++ HealEat/Source/ViewController/Home/StoreVC.swift
@@ -159,12 +159,9 @@ extension StoreVC: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
        let marketVC = MarketVC()
        marketVC.param = MarketVC.Param(placeId: storeData[indexPath.row].id)
-       navigationController?.pushViewController(marketVC, animated: true)
-        let marketVC = MarketVC()
-        marketVC.param = MarketVC.Param(placeId: storeData[indexPath.row].id)
-        let nav = UINavigationController(rootViewController: marketVC)
-        nav.modalPresentationStyle = .fullScreen
-        present(nav, animated: true)
+       let nav = UINavigationController(rootViewController: marketVC)
+       nav.modalPresentationStyle = .fullScreen
+       present(nav, animated: true)
 //        navigationController?.pushViewController(marketVC, animated: true)
     }
 }
